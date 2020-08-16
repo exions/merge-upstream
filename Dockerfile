4a4916ebe80af6d9898ff951e1fd76eb154e2563
lcenchew <lcenchew@outlook.com>
2020-08-16 15:30:02 +0800

    feat: Change to using composite run instead of Docker
    https://docs.github.com/en/actions/creating-actions/creating-a-composite-run-
    steps-action

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM exions/github-actions-git:latest
-
-COPY entrypoint.sh /entrypoint.sh
-
-RUN chmod u+x /entrypoint.sh
-
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -eux
-
-cd "${GITHUB_WORKSPACE}"
-
-git remote add -f upstream "https://github.com/$1.git"
-git merge --ff-only upstream/$2
-git push 
